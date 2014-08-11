b73234af3e3861f68facb90dfb029dc3f620591a
Thomas Khyn <thomas@ksytek.com>
2014-08-11 16:58:08 +1200

    Added test to check ngettext support

--- tests/find.py
+++ tests/find.py
@@ -5,4 +5,14 @@ class FindLazyTests(TestCase):
 
     def test_find_lazy(self):
         self.call_command(['_l'])
-        self.assertInPoFile('This is a lazily translated string')
+        self.assertInPoFile('msgid "This is a lazily translated string"')
+
+
+class FindNGettextTests(TestCase):
+
+    def test_find_ngettext(self):
+        self.call_command(['_n:1,2'])
+
+        self.assertInPoFile('msgid "This is a translated string using singular"')
+        self.assertInPoFile('msgid_plural '
+                            '"This is a translated string using plural"')
